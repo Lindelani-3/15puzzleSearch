b57599efc1e8ebbca1e135e37f3c195b03c9b35b
Developer Delisa <delisamav3@gmail.com>
2023-08-13 12:16:21 +0200

    removed invalid files

--- a star h2/main.cpp
+++ a star h2/main.cpp
@@ -4,14 +4,18 @@
 #include <unordered_set>
 #include <algorithm>
 
+int count = 0;
+// The goal state
 const std::string goalstate = "ABCDEFGHIJKLMNO#";
 
+// Define the node structure for A* algorithm
 struct Node {
-    std::string state;
-    int g_start_to_curr;
-    int h_curr_to_goal;
+    std::string state;  // The current state
+    int g_start_to_curr;  // The actual cost from the start state to this state
+    int h_curr_to_goal;  // The heuristic cost from this state to the goal state
 };
 
+// Calculate the heuristic cost (manhattan distance)
 int manhattan_distance(const std::string& state) {
     int cost = 0;
     for (int i = 0; i < 16; ++i) {
@@ -25,77 +29,100 @@ int manhattan_distance(const std::string& state) {
     return cost;
 }
 
+// Define the comparison function for the priority queue
 struct Compare {
     bool operator()(const Node& a, const Node& b) {
         return a.g_start_to_curr + a.h_curr_to_goal > b.g_start_to_curr + b.h_curr_to_goal;
     }
 };
 
-void add_neighbor(std::priority_queue<Node, std::vector<Node>, Compare>& pq, const Node& current, const std::string& neighbor, std::unordered_set<std::string>& visited) {
-    if(visited.count(neighbor)) return;
+// Add a valid neighboring state to the priority queue
+void add_neighbor(std::priority_queue<Node, std::vector<Node>, Compare>& pq, const Node& current, const std::string& neighbor) {
     int g = current.g_start_to_curr + 1;
     int h = manhattan_distance(neighbor);
     pq.push({neighbor, g, h});
 }
 
-void generate_neighbors(std::priority_queue<Node, std::vector<Node>, Compare>& pq, const Node& current, std::unordered_set<std::string>& visited) {
+// Generate all valid neighboring states and add them to the priority queue
+void generate_neighbors(std::priority_queue<Node, std::vector<Node>, Compare>& pq, const Node& current) {
     std::string state = current.state;
-    int pos = state.find('#');
+    int pos = state.find('#');  // Find the position of the blank space
 
-    int x = pos % 4;
-    int y = pos / 4;
+    int x = pos % 4;  // Column of the blank space
+    int y = pos / 4;  // Row of the blank space
 
+    // If the blank space can be moved up, generate the corresponding neighbor
     if (y > 0) {
         std::string neighbor = state;
         std::swap(neighbor[pos], neighbor[pos - 4]);
-        add_neighbor(pq, current, neighbor, visited);
+        add_neighbor(pq, current, neighbor);
     }
 
+    // If the blank space can be moved down, generate the corresponding neighbor
     if (y < 3) {
         std::string neighbor = state;
         std::swap(neighbor[pos], neighbor[pos + 4]);
-        add_neighbor(pq, current, neighbor, visited);
+        add_neighbor(pq, current, neighbor);
     }
 
+    // If the blank space can be moved left, generate the corresponding neighbor
     if (x > 0) {
         std::string neighbor = state;
         std::swap(neighbor[pos], neighbor[pos - 1]);
-        add_neighbor(pq, current, neighbor, visited);
+        add_neighbor(pq, current, neighbor);
     }
 
+    // If the blank space can be moved right, generate the corresponding neighbor
     if (x < 3) {
         std::string neighbor = state;
         std::swap(neighbor[pos], neighbor[pos + 1]);
-        add_neighbor(pq, current, neighbor, visited);
+        add_neighbor(pq, current, neighbor);
     }
 }
 
-int a_star_manhattan_distance(const std::string& startstate){
+
+
+int a_star_misplaced_tiles(const std::string& startstate){
     std::priority_queue<Node, std::vector<Node>, Compare> pq;
     std::unordered_set<std::string> visited;
 
+    // Push the start state to the priority queue
     pq.push({startstate, 0, manhattan_distance(startstate)});
-    visited.insert(startstate);
 
     while (!pq.empty()) {
         Node current = pq.top();
         pq.pop();
+        count++;
+        // std::cout<<current.state<<std::endl;
 
         if (current.state == goalstate) {
-            std::cout << current.g_start_to_curr << std::endl;
+            // Found the goal state, print the cost and return
+            // std::cout << current.g_start_to_curr << std::endl;
             return current.g_start_to_curr;
         }
 
-        generate_neighbors(pq, current, visited);
+        if (visited.count(current.state)) {
+            // This state has been visited before, skip it
+            continue;
+        }
+
+        visited.insert(current.state);
+
+        // Generate all valid neighboring states and add them to the priority queue
+        generate_neighbors(pq, current);
+
     }
 
     std::cout << "No solution" << std::endl;
     return -1;
 }
 
+
+
 int main() {
     std::string start;
     std::cin >> start;
 
-    std::cout << a_star_manhattan_distance(start) << std::endl;
+    std::cout << a_star_misplaced_tiles(start) << std::endl;
+    // std::cout << count << '\n';
 }
